689f9eac647de17ef95297b416067c03cb59fbc2
surajyadav1975fk <ysuraj1191@gmail.com>
2024-11-15 04:33:19 +0530

    deploy

--- backend/app.js
+++ backend/app.js
@@ -11,7 +11,7 @@ const app=express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://contact-management-application-uytl.vercel.app',
     credentials: true, 
     methods: 'GET,POST,PUT,DELETE,PATCH,OPTIONS',
     allowedHeaders: 'Content-Type, Authorization',
